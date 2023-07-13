f8e8b115a8449191beb492b97eddd4976dfb2ec7
Savannah Shifflet <savannah.shifflet@gmail.com>
2023-07-13 13:46:19 -0500

    added skill fetch

---
+++ assets/script.js
@@ -0,0 +1,56 @@
+var skill = false;
+var trait = false;
+var equipment = false; 
+
+var randomSkill = '';
+var randomEquipment = '';
+var randomTrait = '';
+
+if (skill) {
+    fetch ('https://www.dnd5eapi.co/api/skills', {
+        headers: {"Accept": "application/json"}
+    })
+        .then(function (response) {
+            return response.json();
+        })
+            .then(function (data) {
+                console.log(data.results.length);
+                var skillIndex = parseInt(Math.floor(Math.random() * data.results.length));
+                console.log(skillIndex);
+                randomSkill = data.results[skillIndex].name;
+                console.log(randomSkill);
+            });
+}
+
+if (equipment) {
+    fetch ('https://www.dnd5eapi.co/api/equipment', {
+        headers: {"Accept": "application/json"}
+    })
+        .then(function (response) {
+            return response.json();
+        })
+            .then(function (data) {
+                console.log(data);
+                var equipmentIndex = parseInt(Math.floor(Math.random() * data.results.length));
+                console.log(equipmentIndex);
+                randomEquipment = data.results[equipmentIndex].name;
+                console.log(randomEquipment);
+            });
+}
+        
+
+if (traits) {
+    fetch ('https://www.dnd5eapi.co/api/traits', {
+        headers: {"Accept": "application/json"}
+    })
+        .then(function (response) {
+            return response.json();
+        })
+            .then(function (data) {
+                console.log(data);
+                var traitsIndex = parseInt(Math.floor(Math.random() * data.results.length));
+                console.log(traitsIndex);
+                randomTrait = data.results[traitsIndex].name;
+                console.log(randomTrait);
+            });
+}
